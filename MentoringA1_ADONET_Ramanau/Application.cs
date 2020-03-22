3c63e0056c23e0876c70d449793e1024c40703c0
Романов Алексей <neonzhlobin@tut.by>
2020-03-22 22:02:47 +0300

    Draft commit only to send code to another pc

--- MentoringA1_ADONET_Ramanau/Application.cs
+++ MentoringA1_ADONET_Ramanau/Application.cs
@@ -1,10 +1,47 @@
-﻿namespace MentoringA1_ADONET_Ramanau
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MentoringA1_ADONET_Ramanau
 {
     public class Application
     {
         public void Start()
         {
+            var connectionString = "Data Source=EPBYMINW9787;Initial Catalog=Northwind;Integrated Security=True";
 
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var insertQuery = "INSERT INTO tblUsers VALUES('Ivan2', 66, 'Gomel')";
+                var updateQuery = "UPDATE tblUsers2 SET UserName = 'Anya' WHERE Id = 5";
+                var deleteQuery = "DELETE FROM tblUsers WHERE Id = 5";
+
+                connection.Open();
+                var tran = connection.BeginTransaction();
+                try
+                {
+                    var insertCommand = connection.CreateCommand();
+                    insertCommand.CommandText = insertQuery;
+                    insertCommand.Transaction = tran;
+
+                    var updateCommand = connection.CreateCommand();
+                    updateCommand.CommandText = updateQuery;
+                    updateCommand.Transaction = tran;
+
+                    var deleteCommand = connection.CreateCommand();
+                    deleteCommand.CommandText = deleteQuery;
+                    deleteCommand.Transaction = tran;
+
+                    insertCommand.ExecuteNonQuery();
+                    updateCommand.ExecuteNonQuery();
+                    deleteCommand.ExecuteNonQuery();
+
+                    tran.Commit();
+                }
+                catch (Exception ex)
+                {
+                    tran.Rollback();
+                }
+            }
         }
     }
 }
